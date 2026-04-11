66841ca8f3543756d284c61fe29c465001b7ec24
Syedkhasim786 <syedkhasim2001@gmail.com>
2026-04-11 13:58:21 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import requests
+
+st.title("🧠 AI Price Optimization Assistant")
+
+product = st.text_input("Product Name")
+cost = st.number_input("Cost Price", value=1000)
+competitor = st.number_input("Competitor Price", value=1500)
+demand = st.selectbox("Demand", ["low", "medium", "high"])
+stock = st.selectbox("Stock", ["low", "medium", "high"])
+
+if st.button("Optimize Price"):
+    
+    url = "http://localhost:8000/optimize-price"  # will fix later after deployment
+    
+    params = {
+        "product": product,
+        "cost": cost,
+        "competitor": competitor,
+        "demand": demand,
+        "stock": stock
+    }
+
+    response = requests.post(url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        st.success(f"Recommended Price: ₹{data['recommended_price']}")
+        st.write(data["explanation"])
+    else:
+        st.error("Error fetching result")
